3f873f401186a57385547f5350f2cafedfa8aa91
Sanjaymark16 <sanjaymark16@gmail.com>
2023-07-11 12:29:52 +0530

    BackEnd Day2

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs")
+//initlising server
+const app = express();
 
-//initializing server
-const app = express()
-const PORT = 8081;
+const PORT = 8080;
+
+//Server endpoint
+app.get("/", (req, res)=>{
+  let date = new Date();
+  let content = date.toUTCString();
+  fs.writeFile("./currentTime.txt",content , (err)=>{
+      if(err){
+        res.send("Error Occured", err);
+      }else {
+        fs.readFile("./currentTime.txt","utf-8", (err, data) => {
+          if (err) {
+             res.send(`Error occured in reading : ${err}`);
+          } else {
+            res.send(data);
+          }
+        } )
+      }
+  }); 
 
 
-//SERVER ENDPOINT
-app.get("/",(req, res)=>
-{
-    let date = new Date();
-    let content = date.toUTCString();
-    fs.writeFile("./Timestamp/date-time.txt",content, (err)=>
-    {
-        if(err)
-        {
-            res.send("Error Occured", err);
-        }
-        else
-        {
-            fs.readFile("./Timestamp/date-time.txt","utf-8", (err, data)=>
-            {
-                if(err)
-                {
-                    res.send("Error:",err);
-                }
-                else
-                {
-                    res.send(data);
-                }
-            })
-        }
-    })
 })
 
-
-
-
 //start the server
-app.listen(PORT, ()=>console.log(`Server started in localhost:${PORT}`))
+app.listen(PORT, ()=>console.log(`Server started in localhost:${PORT}`));
